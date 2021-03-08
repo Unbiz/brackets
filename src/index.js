9fbd55907fb015fccbe0535103f8dca3874f475b
Vladimir Bessonov <bessmy@gmail.com>
2021-03-08 14:59:57 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const flatConfig = bracketsConfig.flat();
+  const strArr = str.split('');
+  const stack = [];
+
+  for (let i = 0; i < strArr.length; i += 1) {
+    const bracket = strArr[i];
+    const bracketIndex = flatConfig.indexOf(bracket);
+
+    if (bracketIndex % 2 === 0) {
+      if (bracket === flatConfig[bracketIndex + 1] && stack[stack.length - 1] === bracketIndex) {
+        stack.pop();
+      } else {
+        stack.push(bracketIndex);
+      }
+    } else {
+      if (stack.pop() !== bracketIndex - 1) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
